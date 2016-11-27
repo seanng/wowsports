b9937493d972c5ecdb5d9b37f4159dbecd404629
Sean <shonum@gmail.com>
2016-11-26 16:41:52 -0800

    Complete heading section

---
+++ src/playersCarousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $("#players-content").owlCarousel({
+    items: ( ($(window).width() < 767) ? 1 : 3 ),
+    autoPlay: true,
+    loop: true,
+    autoplayHoverPause: true,
+    autoPlaySpeed: 3000
+  });
+
+});
