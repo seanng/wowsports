a1d644def2b2351b3fd150709db21ec6fb237638
Sean <shonum@gmail.com>
2016-11-26 19:38:36 -0800

    Complete project

---
+++ src/scrollEffect.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 350) {
+      $('nav').addClass('nav-red');
+    } else {
+      $('nav').removeClass('nav-red');
+    }
+  });
+});
